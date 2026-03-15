8baf872bbed45d9d184cda812c8da529d7bef023
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-03-15 22:04:50 +0100

    Add files

--- 20level/fromto.go
+++ 20level/fromto.go
@@ -9,50 +9,22 @@ func main() {
 	fmt.Print(FromTo(100, 10))
 }
 
-func FromTo(from int, to int) string {
-    // Check if numbers are within valid range (0-99)
-    if from < 0 || from > 99 || to < 0 || to > 99 {
-        return "Invalid\n"
-    }
-    
-    result := ""
-    
-    if from <= to {
-        // Count UP (including when from == to)
-        for i := from; i <= to; i++ {
-            // Add formatted number to result
-            result += formatNumber(i)
-            
-            // Add comma and space if not the last number
-            if i < to {
-                result += ", "
-            }
-        }
-    } else {
-        // Count DOWN
-        for i := from; i >= to; i-- {
-            // Add formatted number to result
-            result += formatNumber(i)
-            
-            // Add comma and space if not the last number
-            if i > to {
-                result += ", "
-            }
-        }
-    }
-    
-    // Add newline at the end
-    return result + "\n"
-}
+func FromTo(f, t int) string {
+	if f < 0 || f > 99 || t < 0 || t > 99 {
+		return "Invalid\n"
+	}
 
-// Helper function to format a number with leading zero if needed
-func formatNumber(num int) string {
-    if num < 10 {
-        // Manually create "0X" format
-        return string(rune('0' + num/10)) + string(rune('0' + num%10))
-    }
-    // Convert numbers 10-99 to string manually
-    tens := num / 10
-    ones := num % 10
-    return string(rune('0' + tens)) + string(rune('0' + ones))
+	s, step := "", 1
+	if f > t {
+		step = -1
+	}
+
+	for i := f; ; i += step {
+		s += string('0'+i/10) + string('0'+i%10)
+		if i == t {
+			break
+		}
+		s += ", "
+	}
+	return s + "\n"
 }
--- 20level/lastword.go
+++ 20level/lastword.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-        fmt.Print(LastWord("FOR PONY"))
-        fmt.Print(LastWord("this        ...       is sparta, then again, maybe    not"))
-        fmt.Print(LastWord("  "))
-        fmt.Print(LastWord("  lorem,ipsum  "))
+	fmt.Print(LastWord("FOR PONY"))
+	fmt.Print(LastWord("this        ...       is sparta, then again, maybe    not"))
+	fmt.Print(LastWord("  "))
+	fmt.Print(LastWord("  lorem,ipsum  "))
 }
 
 func LastWord(s string) string {
-i := len(s) - 1
-for i >= 0 && s[i] == ' ' {
-i--
-}
-end := i
-for i >= 0 && s[i] != ' ' {
-i--
-}
-return s[i+1:end+1] + "\n"
+	i := len(s) - 1
+	for i >= 0 && s[i] == ' ' {
+		i--
+	}
+	end := i
+	for i >= 0 && s[i] != ' ' {
+		i--
+	}
+	return s[i+1:end+1] + "\n"
 }
