dfa6acb1cd75dc652a22096fade3c883befb360c
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-03-15 22:45:17 +0100

    Modify file

--- 20level/printmemory.go
+++ 20level/printmemory.go
@@ -11,21 +11,25 @@ func hex(b byte) {
 	z01.PrintRune(rune(h[b/16]))
 	z01.PrintRune(rune(h[b%16]))
 }
+
 func PrintMemory(a [10]byte) {
 	for i := 0; i < 10; i++ {
 		hex(a[i])
-		if i%4 == 3 {
+		if (i+1)%4 == 0 {
 			z01.PrintRune('\n')
 		} else {
 			z01.PrintRune(' ')
 		}
 	}
-	for _, v := range a {
-		if v >= 32 && v <= 126 {
-			z01.PrintRune(rune(v))
+
+	for i := 0; i < 10; i++ {
+		if a[i] >= 32 && a[i] <= 126 {
+			z01.PrintRune(rune(a[i]))
 		} else {
 			z01.PrintRune('.')
 		}
+		if (i+1)%4 == 0 {
+			z01.PrintRune('\n')
+		}
 	}
-	z01.PrintRune('\n')
 }
