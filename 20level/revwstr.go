fef4d4486de4cd772c2b556d6b10fe3155ffee70
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-03-16 08:56:25 +0100

    Add file

---
+++ 20level/revwstr.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Test prints
+	fmt.Println(revwstr("the time of contempt precedes that of indifference"))
+	fmt.Println(revwstr("abcdefghijklm"))
+	fmt.Println(revwstr("he stared at the mountain"))
+	fmt.Println(revwstr(""))
+}
+
+func revwstr(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	// Count words
+	words := []string{}
+	start := 0
+	for i := 0; i <= len(s); i++ {
+		if i == len(s) || s[i] == ' ' {
+			if start < i {
+				words = append(words, s[start:i])
+			}
+			start = i + 1
+		}
+	}
+
+	// Build reversed string
+	result := ""
+	for i := len(words) - 1; i >= 0; i-- {
+		if i < len(words)-1 {
+			result += " "
+		}
+		result += words[i]
+	}
+
+	return result
+}
