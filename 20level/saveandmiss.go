71b22b69371c3b8cea2a321a2cb9b647dec721c3
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-03-16 05:24:27 +0100

    Add files

---
+++ 20level/saveandmiss.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(SaveAndMiss("123456789", 3))
+	fmt.Println(SaveAndMiss("abcdefghijklmnopqrstuvwyz", 3))
+	fmt.Println(SaveAndMiss("", 3))
+	fmt.Println(SaveAndMiss("hello you all ! ", 0))
+	fmt.Println(SaveAndMiss("what is your name?", 0))
+	fmt.Println(SaveAndMiss("go Exercise Save and Miss", -5))
+}
+
+func SaveAndMiss(s string, n int) string {
+	if n <= 0 {
+		return s
+	}
+	res := ""
+	save := true
+	count := 0
+	for _, r := range s {
+		if save {
+			res += string(r)
+		}
+		count++
+		if count == n {
+			save = !save
+			count = 0
+		}
+	}
+	return res
+}
