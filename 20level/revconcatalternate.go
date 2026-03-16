e4727fd6d1f1f3c49f4ae37693bc608921755202
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-03-16 05:20:20 +0100

    Add file

---
+++ 20level/revconcatalternate.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3, 9, 8}, []int{4, 5}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{}))
+}
+
+func RevConcatAlternate(slice1, slice2 []int) []int {
+	result := []int{}
+	len1, len2 := len(slice1), len(slice2)
+
+	if len1 > len2 {
+		// Add extra elements from larger slice (slice1)
+		for i := len1 - 1; i >= len2; i-- {
+			result = append(result, slice1[i])
+		}
+		// Alternate remaining elements
+		for i := len2 - 1; i >= 0; i-- {
+			result = append(result, slice1[i])
+			result = append(result, slice2[i])
+		}
+	} else if len2 > len1 {
+		// Add extra elements from larger slice (slice2)
+		for i := len2 - 1; i >= len1; i-- {
+			result = append(result, slice2[i])
+		}
+		// Alternate remaining elements
+		for i := len1 - 1; i >= 0; i-- {
+			result = append(result, slice1[i])
+			result = append(result, slice2[i])
+		}
+	} else {
+		// Equal length - alternate starting with first slice
+		for i := len1 - 1; i >= 0; i-- {
+			result = append(result, slice1[i])
+			result = append(result, slice2[i])
+		}
+	}
+
+	return result
+}
