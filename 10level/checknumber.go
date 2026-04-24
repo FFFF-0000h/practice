b8aa289c68b778c885bfa66d766e2151d6d4577d
FFFF-0000h <adebiyiitunuayo@gmail.com>
2026-04-24 13:16:29 +0100

    Add new files

--- 10level/checknumber.go
+++ 10level/checknumber.go
@@ -12,14 +12,28 @@ func main() {
 }
 
 func CheckNum(s string) bool {
-	r := []rune(s)
+	r := []rune(s) // Convert the string to a slice of runes
 	for i := 0; i < len(r); i++ {
+		// Iterate over the slice of runes
 		if r[i] >= '0' && r[i] <= '9' {
 			return true
 		}
 	}
 	return false
 }
+
+/*
+func main() {
+	str := "Hello"
+	r := []rune(str)
+	for i := 0; i < len(r); i++ {
+		if r[i] >= '0' && r[i] <= '9' {
+			fmt.Println(0)
+		}
+	}
+	fmt.Println(1)
+}
+*/
 
 /*
 func CheckNumber(arg string) bool {
--- 10level/countalpha.go
+++ 10level/countalpha.go
@@ -9,8 +9,12 @@ func main() {
 }
 
 func CountAlpha(s string) int {
+	// Convert the string to a rune
 	r := []rune(s)
+	// Set a counter that ticks for every count
 	count := 0
+
+	// Iterate over the slice of runes, neglecting the value
 	for i, _ := range r {
 		if r[i] >= 'a' && r[i] <= 'z' || r[i] >= 'A' && r[i] <= 'Z' {
 			count++
